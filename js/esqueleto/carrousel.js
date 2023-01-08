cb7180ae3dc7c05fcf58db3e09fe0ccc16b47faa
aceros123 <57160341+aceros123@users.noreply.github.com>
2023-01-08 15:47:29 -0500

    cambio en el carrusel

---
+++ js/esqueleto/carrousel.js
@@ -0,0 +1,10 @@
+const carrousel = document.getElementById('imagenes')
+
+// $(carrousel).append(`
+// <li><img src="../../assets/slider/1.jpg" alt="" title="" id="wows1_0" /></li>
+//                 <li><img src="../../assets/slider/2.jpg" alt="" title="" id="wows1_1" /></li>
+//                 <li><a href="http://wowslider.com/vi"><img src="../../assets/slider/3.jpg" alt="bootstrap slider" title="" id="wows1_2" /></a></li>
+//                 <li><img src="../../assets/slider/4.jpg" alt ="" title="" id="wows1_3" /></li>
+//                 <li><img src="../../assets/slider/7.jpg" alt="" title="" id="wows1_4" /></li>
+//                 <li><img src="../../assets/slider/6.jpg" alt="" title="" id="wows1_5" /></li>
+// `);
