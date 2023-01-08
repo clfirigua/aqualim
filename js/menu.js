182cd3fc0eeaf88339fce77c8e02c94425fa64a4
aceros123 <57160341+aceros123@users.noreply.github.com>
2023-01-08 15:25:18 -0500

    se realiza cambio en el menu del proyecto
    se remplaza el menu general repetitivo. se crea un archivo en la carpeta de JS llamado menu, donde se crea el menu y se inyecta al html por jquery

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+const menu =  document.getElementById('menu')
+
+$(menu).append(
+    `<div id='menuLogo'>
+        <img class="img-rounded img-responsive" data-src="holder.js/900x500/auto" alt="900x500" src="../assets/images/aqualim.jpg">
+    </div>
+
+    <div class="container">
+
+        <a class="toggleMenu" href="#" style="display: none">Menu</a>
+        <ul class="nav">
+            <li class="test">
+            <li><a href=""><span> </span></a></li>
+            <li><a href="../index.html"><span>Inicio</span></a></li>
+            <li><a href="../pages/servicios.html"><span class='down'>Servicios</span></a></li>
+            <li class='top'><a class='top_link' href="../pages/correo.html"><span class='down'>Contáctenos </span></a></li>
+            <li class='top'><a class='top_link' href="../pages/califiquenos.html"><span class='down'>Calífiquenos</span></a></li>
+            </li>
+        </ul>
+    </div>`
+)
