388d97d2423e9d42b0aee1346cae33e0d42a0fc0
aceros123 <57160341+aceros123@users.noreply.github.com>
2023-04-10 22:28:45 -0500

    Update menu.js
    salir a la raiz

--- js/esqueleto/menu.js
+++ js/esqueleto/menu.js
@@ -10,7 +10,7 @@ $(menu).append(
         <a class="toggleMenu" href="#" style="display: none">Menu</a>
         <ul class="nav">
 
-            <li><a href="../index.html"><span>Inicio</span></a></li>
+            <li><a href="../"><span>Inicio</span></a></li>
             <li><a href="../pages/servicios.html"><span class='down'>Servicios</span></a></li>
             <li class='top'><a class='top_link' href="../pages/correo.html"><span class='down'>Contáctenos </span></a></li>
             <li class='top'><a class='top_link' href="../pages/califiquenos.html"><span class='down'>Calífiquenos</span></a></li>
